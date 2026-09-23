79f991b3385b7382a6b014895e06134fe74d5f93
Javokhir Kubaev <65939675+thedreamJK7@users.noreply.github.com>
2026-09-23 15:38:12 +0200

    Add: two-integer-sum-ii - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-8.cpp
@@ -0,0 +1,23 @@
+#pragma optimize("OFast","unroll-loops");
+
+static const auto fast = []() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0;
+        int r = numbers.size() - 1;
+        while (r > l) {
+            if (numbers[l] + numbers[r] == target) 
+                return {l + 1, r + 1};
+            else if (numbers[l] + numbers[r] < target) 
+                l++;
+            else 
+                r--;
+        }
+    }
+};
