6225be9e5ec92be81147dee0f3715bbd888b94db
Javokhir Kubaev <65939675+thedreamJK7@users.noreply.github.com>
2026-09-24 11:08:56 +0200

    Add: max-water-container - submission-8

---
+++ Data Structures & Algorithms/max-water-container/submission-8.cpp
@@ -0,0 +1,26 @@
+static const auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int res = INT_MIN;
+        int l = 0;
+        int r = heights.size() - 1;
+        while (l < r) {
+            int s = min(heights[l], heights[r]) * (r - l);
+            if (res < s) {
+                res = s;
+            }
+            if (heights[l] > heights[r]) {
+                r--;
+            } else {
+                l++;
+            }
+        }
+        return res;
+    }
+};
